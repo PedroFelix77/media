a6d3a0573771d12c790f07529ee456543824adfc
Pedro Henrique Felix Cavalcanti <pedrohfcavalcanti7@gmail.com>
2026-03-17 09:49:15 -0300

    ajustando node modules

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { config } = require('./config');
-const { logger } = require('./logger');
-const { startWorkers, closeWorkers } = require('./mediasoup/workers');
-const { startHttpServer } = require('./http/httpServer');
-const { startWsServer } = require('./ws/wsServer');
+const { config } = require("./config");
+const { logger } = require("./logger");
+const { startWorkers, closeWorkers } = require("./mediasoup/workers");
+const { startHttpServer } = require("./http/httpServer");
+const { startWsServer } = require("./ws/wsServer");
 
 async function main() {
   if (!config.auth.jwtSecret) {
-    throw new Error('SFU_JWT_SECRET não configurado.');
+    throw new Error("SFU_JWT_SECRET não configurado.");
   }
   if (!config.http.apiSecret) {
-    throw new Error('SFU_API_SECRET não configurado.');
+    throw new Error("SFU_API_SECRET não configurado.");
   }
 
   logger.info(
@@ -19,9 +19,9 @@ async function main() {
       nodeId: config.nodeId,
       httpPort: config.http.port,
       wsPort: config.ws.port,
-      numWorkers: config.mediasoup.numWorkers
+      numWorkers: config.mediasoup.numWorkers,
     },
-    'SFU iniciando'
+    "SFU iniciando",
   );
 
   await startWorkers();
@@ -30,27 +30,26 @@ async function main() {
   const wsServer = await startWsServer();
 
   const shutdown = async (signal) => {
-    logger.info({ signal }, 'SFU encerrando');
+    logger.info({ signal }, "SFU encerrando");
     try {
       wsServer.close();
     } catch (e) {
-      logger.warn({ err: e }, 'Falha ao fechar WS');
+      logger.warn({ err: e }, "Falha ao fechar WS");
     }
     try {
       httpServer.close();
     } catch (e) {
-      logger.warn({ err: e }, 'Falha ao fechar HTTP');
+      logger.warn({ err: e }, "Falha ao fechar HTTP");
     }
     await closeWorkers();
     process.exit(0);
   };
 
-  process.on('SIGINT', () => shutdown('SIGINT'));
-  process.on('SIGTERM', () => shutdown('SIGTERM'));
+  process.on("SIGINT", () => shutdown("SIGINT"));
+  process.on("SIGTERM", () => shutdown("SIGTERM"));
 }
 
 main().catch((err) => {
-  logger.error({ err }, 'Falha fatal ao iniciar SFU');
+  logger.error({ err }, "Falha fatal ao iniciar SFU");
   process.exit(1);
 });
-
